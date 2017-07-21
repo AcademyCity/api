19fdfc8c77b2cdcd5df4b13e07d07939a84def11
Toki <Toki.Qi@outlook.com>
2017-07-21 19:08:12 +0800

    接口认证修改

--- Core/Authorize/RequestAuthorizeAttribute.cs
+++ Core/Authorize/RequestAuthorizeAttribute.cs
@@ -22,30 +22,34 @@ namespace Core.Authorize
         //重写基类的验证方式，加入我们自定义的Ticket验证
         public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
         {
-            //从http请求的头里面获取身份验证信息，验证是否是请求发起方的ticket
-            var authorization = actionContext.Request.Headers.Authorization;
-            if ((authorization != null) && (authorization.Parameter != null))
+            //是否允许匿名访问，则返回未验证401
+            var attributes = actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().OfType<AllowAnonymousAttribute>();
+            bool isAnonymous = attributes.Any(a => a is AllowAnonymousAttribute);
+            if (isAnonymous)
             {
-                //解密用户ticket,并校验用户名密码是否匹配
-                var encryptTicket = authorization.Parameter;
-                if (ValidateTicket(encryptTicket))
+                //从http请求的头里面获取身份验证信息，验证是否是请求发起方的ticket
+                var authorization = actionContext.Request.Headers.Authorization;
+                if ((authorization != null) && (authorization.Parameter != null))
                 {
-                    base.IsAuthorized(actionContext);
+                    //解密用户ticket,并校验用户名密码是否匹配
+                    var encryptTicket = authorization.Parameter;
+                    if (ValidateTicket(encryptTicket))
+                    {
+                        base.IsAuthorized(actionContext);
+                    }
+                    else
+                    {
+                        HandleUnauthorizedRequest(actionContext);
+                    }
                 }
                 else
                 {
-                    HandleUnauthorizedRequest(actionContext);
+                    base.OnAuthorization(actionContext);
                 }
             }
-            //如果取不到身份验证信息，并且不允许匿名访问，则返回未验证401
             else
             {
-                var attributes = actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().OfType<AllowAnonymousAttribute>();
-                bool isAnonymous = attributes.Any(a => a is AllowAnonymousAttribute);
-                if (isAnonymous)
-                    base.OnAuthorization(actionContext);
-                else
-                    HandleUnauthorizedRequest(actionContext);
+                HandleUnauthorizedRequest(actionContext);
             }
         }
 
@@ -54,26 +58,17 @@ namespace Core.Authorize
         //校验用户名密码（正式环境中应该是数据库校验）
         private bool ValidateTicket(string encryptTicket)
         {
-            try
-            {
-                //解密Ticket
-                string openId = FormsAuthentication.Decrypt(encryptTicket).UserData;
+            //解密Ticket
+            string openId = FormsAuthentication.Decrypt(encryptTicket).UserData;
 
-                if (openId == "")
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            catch(Exception e)
+            if (openId == "")
             {
-                Log.Error("xx");
+                return true;
+            }
+            else
+            {
                 return false;
             }
-            
         }
 
         protected override void HandleUnauthorizedRequest(HttpActionContext filterContext)
